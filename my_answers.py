48ef521db1f803371a3ef5e4b3f4acff5c06dc11
Jaehwan Park <turnaround0@gmail.com>
2019-03-18 21:42:10 +0900

    Add works for RNN project
    This is for second project of Udacity AI part2.

--- my_answers.py
+++ my_answers.py
@@ -1,46 +1,74 @@
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Activation
 from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# Transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
-    X = []
-    y = []
+    X = [series[i: i + window_size] for i in range(0, len(series) - window_size)]
+    y = series[window_size:]
 
     # reshape each 
     X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
+    X.shape = (np.shape(X)[0: 2])
     y = np.asarray(y)
-    y.shape = (len(y),1)
+    y.shape = (len(y), 1)
 
-    return X,y
+    return X, y
 
-# TODO: build an RNN to perform regression on our time series input/output data
+# An RNN to perform regression on our time series input/output data
+# layer 1 uses an LSTM module with 5 hidden units (note here the input_shape = (window_size,1))
+# layer 2 uses a fully connected module with one unit
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    hidden_units = 5
+    model.add(LSTM(hidden_units, input_shape=(window_size, 1)))
+    model.add(Dense(1, activation='linear'))
+    return model
 
 
-### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+# Return the text input with only ascii lowercase and the given punctuation
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
 
+    text = text.lower()
+    chars = set(text)
+
+    for char in chars:
+        # do not allow extended ascii characters
+        if not char.encode('utf-8').isalpha() and char not in punctuation:
+            text = text.replace(char, ' ')
+
     return text
 
-### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+# Transform the input text
+# and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
 
-    return inputs,outputs
+    for i in range(0, len(text) - window_size, step_size):
+        inputs.append(text[i: i + window_size])
+        outputs.append(text[i + window_size])
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+    return inputs, outputs
+
+# 3 layer RNN model
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
+# layer 1 should be an LSTM module with 200 hidden units
+#  --> note this should have input_shape = (window_size, len(chars))
+# layer 2 should be a linear module, fully connected, with len(chars) hidden units
+# layer 3 should be a softmax activation (solving a multiclass classification)
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    hidden_units = 200
+    model.add(LSTM(hidden_units, input_shape=(window_size, num_chars)))
+    model.add(Dense(num_chars, activation='linear'))
+    model.add(Activation('softmax'))
+    return model
